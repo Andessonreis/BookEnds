511aac1b55cda4987a66098c7076600d801e99ab
Andesson reis <Andesson reis@DESKTOP-PSC4KAO>
2023-05-09 14:52:44 -0300

    Added Spring MVC

--- src/main/java/br/com/ifba/BookEnds/config/WebMvcConfig.java
+++ src/main/java/br/com/ifba/BookEnds/config/WebMvcConfig.java
@@ -4,10 +4,30 @@
  */
 package br.com.ifba.BookEnds.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  *
  * @author Andesson reis
  */
-public class WebMvcConfig {
-    
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("br.com.ifba.library.bookController")
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp().prefix("/WEB-INF/views/").suffix(".jsp");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+    }
 }
